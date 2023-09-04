f33fc47218a6ccf3f1d4d35e599f828e7d2c2640
MrSuit <141973883+MrSuit@users.noreply.github.com>
2023-09-04 19:39:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+mX=0;
+mY=0;
+
+function preload() {
+    mustache = loadImage('m.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(mustache, mX-10, mY+5, 30, 30)
+}
+
+function take_snapshot(){
+    save('myFilterImage.png')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        mX = results[0].pose.nose.x;
+        mY = results[0].pose.nose.y;
+        console.log("nose x:" + results[0].pose.nose.x);
+        console.log("nose y:" + results[0].pose.nose.y);
+    }
+}
